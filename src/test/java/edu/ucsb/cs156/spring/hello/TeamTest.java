91de14ff34c7d08836498b3172e7a350d3350ac0
Arjun Shah <arjun_shah@ucsb.edu>
2026-04-09 18:02:31 -0700

    Update TeamTest.java

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -26,4 +26,34 @@ public class TeamTest {
     }
 
 
+
+        @Test
+    public void equals_same_object_returns_true() {
+        assertEquals(true, team.equals(team));
+    }
+
+    @Test
+    public void equals_different_class_returns_false() {
+        assertEquals(false, team.equals("not a team"));
+    }
+
+    @Test
+    public void equals_same_name_same_members_returns_true() {
+        Team team2 = new Team("test-team");
+        assertEquals(true, team.equals(team2));
+    }
+
+    @Test
+    public void equals_same_name_different_members_returns_false() {
+        Team team2 = new Team("test-team");
+        team2.addMember("Alice");
+        assertEquals(false, team.equals(team2));
+    }
+
+    @Test
+    public void equals_different_name_returns_false() {
+        Team team2 = new Team("other-team");
+        assertEquals(false, team.equals(team2));
+    }
+
 }
