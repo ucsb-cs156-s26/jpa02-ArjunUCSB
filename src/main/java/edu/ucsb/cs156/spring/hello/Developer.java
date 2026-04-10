8ce78a8682f59c2f8e61273d2115dcd48fccc9bb
Arjun Shah <arjun_shah@ucsb.edu>
2026-04-09 17:33:18 -0700

    Update team members in Developer.java

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -38,12 +38,12 @@ public class Developer {
     public static Team getTeam() {
         // TODO: Change this to your team name
         Team team = new Team("s26-12");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        team.addMember("Arjun M.");
+        team.addMember("Arjun S.");
+        team.addMember("Jay");
+        team.addMember("Lorenzo");
+        team.addMember("Pau");
+        team.addMember("Pengyu");
         return team;
     }
 }
