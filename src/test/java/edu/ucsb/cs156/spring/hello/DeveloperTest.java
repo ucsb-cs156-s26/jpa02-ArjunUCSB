80f324aeb005aebc37b604fae9f9ac6d78c7c3f1
Arjun Shah <arjun_shah@ucsb.edu>
2026-04-09 17:23:59 -0700

    Update DeveloperTest.java

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -22,8 +22,7 @@ public class DeveloperTest {
 
     @Test
     public void getName_returns_correct_name() {
-        // TODO: Replace Chris G. with your name as shown on
-        // <https://bit.ly/cs156-s26-teams>
+
         assertEquals("Arjun S.", Developer.getName());
     }
 
