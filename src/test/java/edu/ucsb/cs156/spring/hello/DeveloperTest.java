f969c4b42e13e6dfd9e0548f6387ed3a8ca7cf2b
Arjun Shah <arjun_shah@ucsb.edu>
2026-04-09 17:28:41 -0700

    Update DeveloperTest.java

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -26,7 +26,8 @@ public class DeveloperTest {
         assertEquals("Arjun S.", Developer.getName());
     }
 
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
-
+    @Test
+public void getGithubId_returns_correct_githubId() {
+    assertEquals("ArjunUCSB", Developer.getGithubId());
+}
 }
